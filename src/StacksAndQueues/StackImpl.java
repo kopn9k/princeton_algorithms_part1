808cf5c6d711c1ce52ee84c132423da44b81b738
Pavel Orashkov <pavelo@playtika.com>
2024-06-09 13:31:49 +0400

    Add stack implementation

---
+++ src/StacksAndQueues/StackImpl.java
@@ -0,0 +1,24 @@
+package StacksAndQueues;
+
+public class StackImpl {
+
+    private class Node {
+        String value;
+        Node next;
+    }
+
+    private Node first = null;
+
+    public void push(String string) {
+        Node oldFirst = first;
+        first = new Node();
+        first.value = string;
+        first.next = oldFirst;
+    }
+
+    public String pop() {
+        String value = first.value;
+        first = first.next;
+        return value;
+    }
+}
